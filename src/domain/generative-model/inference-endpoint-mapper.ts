ca8af7d4b2c8eeda17adcd4940b299e74b287740
Miodrag Vilotijevic <miodragvilotijevic@gmail.com>
2026-06-07 13:03:51 +0200

    pushed unsaved file

--- src/domain/generative-model/inference-endpoint-mapper.ts
+++ src/domain/generative-model/inference-endpoint-mapper.ts
@@ -1,6 +1,8 @@
+import { InferenceResponse } from "@domain/agentic-environment/inference/response"
 import { ModelName } from "./generative-model"
 import { InferenceParams } from "@domain/agentic-environment/inference/params"
 
-export interface ProviderEndpointMapper {
+export interface InferenceEndpointMapper {
 	toRequest(inferenceParams: InferenceParams<ModelName>): any
+	toResponse(response: any): InferenceResponse
 }
