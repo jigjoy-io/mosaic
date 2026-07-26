f43c5d7d694edd099af95471c21c169de47d19a6
Miodrag Vilotijevic <miodragvilotijevic@gmail.com>
2026-07-26 10:45:11 +0200

    register user to runtime (example)

--- examples/main.ts
+++ examples/main.ts
@@ -1,13 +1,15 @@
 import { agent } from "./agent/agent"
 import { ParticipantMessage } from "../src/domain/agentic-environment/events/participant-message"
 import { EnvironmentState, FreemiumAccount, initializeRuntime } from "./runtime"
+import { user } from "./user/user"
 
 const freemiumAccount = FreemiumAccount.init(3)
 const runtimeState = new EnvironmentState(freemiumAccount)
 
 const runtime = initializeRuntime(runtimeState)
 
+runtime.join(user)
 runtime.join(agent)
 
-const participantMessage = ParticipantMessage.create("user", "What is the capital of France?")
+const participantMessage = ParticipantMessage.create(user.getId(), "What is the capital of France?")
 runtime.deliver(participantMessage)
